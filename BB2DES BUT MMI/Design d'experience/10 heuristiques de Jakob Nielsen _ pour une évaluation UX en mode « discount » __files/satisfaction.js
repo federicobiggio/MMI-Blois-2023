45bc4bd86ac52f3883b4a21009ae401567d91d28
federicobiggio <117173826+federicobiggio@users.noreply.github.com>
2023-08-08 11:48:18 +0200

    ok

--- BB2DES BUT MMI/Design d'experience/10 heuristiques de Jakob Nielsen _ pour une évaluation UX en mode « discount » __files/satisfaction.js
+++
@@ -1,6 +0,0 @@
-jQuery(document).ready(function() {
-    var number = jQuery('.radial-progress').data('number');
-    var fill_rotation = 180 * (number / 100);
-    jQuery('.circle .fill, .circle .mask.full').css('transform', 'rotate(' + fill_rotation + 'deg)');
-
-});
